d9fb811f5953050bb34f93d350a78168f9fd0732
ChaJunWoo <wnsdnxla123@gmail.com>
2025-12-17 14:40:23 +0900

    feat: sqlite 연결 및 CRUD 메서드 구현

---
+++ db_manager.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+class DBManager:
+    def __init__(self):
+        self.conn = sqlite3.connect("board.db")
+        self._create_table()
+
+    def _create_table(self):
+        self.conn.execute("""
+            CREATE TABLE IF NOT EXISTS board (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                content TEXT NOT NULL,
+                author TEXT NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+        self.conn.commit()
+
+    def create_post(self, title, content, author):
+        if not title or not content:
+            raise ValueError("title or content cannot be empty")
+
+        cursor = self.conn.execute("""
+            INSERT INTO board (title, content, author)
+            VALUES (?, ?, ?)
+        """,
+        (title, content, author)
+        )
+        self.conn.commit()
+
+        return cursor.lastrowid
+
+    def get_post(self, post_id):
+        cursor = self.conn.execute("""
+            SELECT *
+            FROM board
+            WHERE id = ?
+        """,
+        (post_id,)
+        )
+
+        return cursor.fetchone()
+
+    def get_posts(self):
+        cursor = self.conn.execute("""
+            SELECT *
+            FROM board
+        """)
+
+        return cursor.fetchall()
+
+    def update_post(self, new_title, new_content, post_id):
+        cursor = self.conn.execute("""
+            UPDATE board
+            SET title = ?, content = ?, updated_at = CURRENT_TIMESTAMP
+            WHERE id = ? 
+        """,
+       (new_title, new_content, post_id)
+        )
+        self.conn.commit()
+
+        return cursor.rowcount
+
+    def delete_post(self, post_id):
+        cursor = self.conn.execute("""
+            DELETE FROM board
+            WHERE id = ?
+        """,
+        (post_id,)
+        )
+        self.conn.commit()
+
+        return cursor.rowcount
