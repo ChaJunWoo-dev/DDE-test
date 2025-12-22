d0dcc406cd3fe5f8eb839785498f174022181474
ChaJunWoo <wnsdnxla123@gmail.com>
2025-12-22 20:14:48 +0900

    test: db_manager 조회 테스트 추가

--- test/db_test.py
+++ test/db_test.py
@@ -18,3 +18,16 @@ class TestDBManager:
         post_id = db.create_post(title, content, author)
 
         assert post_id > 0
+
+    def test_get_post(self, db):
+        title = "test2"
+        author = "author"
+        content = "test content"
+
+        post_id = db.create_post(title, content, author)
+        post = db.get_post(post_id)
+
+        assert post is not None
+        assert post.title == "test2"
+        assert post.author == "author"
+        assert post.content == "test content"
