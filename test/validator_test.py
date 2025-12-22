cf0a8c5efeddc4bf529f983376f016650c118bfc
ChaJunWoo <wnsdnxla123@gmail.com>
2025-12-22 21:35:22 +0900

    test: post input validator 실패 케이스 테스트 추가

--- test/validator_test.py
+++ test/validator_test.py
@@ -19,3 +19,28 @@ class TestValidator:
         error_msg = validate_post_input(title, content)
 
         assert error_msg is None
+
+    def test_empty_title(self):
+        title = ""
+        content = "content"
+
+        error_msg = validate_post_input(title, content)
+
+        assert error_msg == Message.TITLE_REQUIRED
+
+    def test_empty_content(self):
+        title = "test"
+        content = ""
+
+        error_msg = validate_post_input(title, content)
+
+        assert error_msg == Message.CONTENT_REQUIRED
+
+    def test_empty_author(self):
+        title = "test"
+        content = "content"
+        author = ""
+
+        error_msg = validate_post_input(title, content, author)
+
+        assert error_msg == Message.AUTHOR_REQUIRED
