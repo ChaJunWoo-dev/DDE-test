8475679ce8d9fb5e692b9a22b722d1df00719428
ChaJunWoo <wnsdnxla123@gmail.com>
2025-12-22 20:14:21 +0900

    test: db_manager 생성 테스트 추가

--- test/db_test.py
+++ test/db_test.py
@@ -9,3 +9,12 @@ class TestDBManager:
         db = DBManager(":memory:")
         yield db
         db.close()
+
+    def test_create_post(self, db):
+        title = "test_title"
+        author = "test_author"
+        content = "this is a test"
+
+        post_id = db.create_post(title, content, author)
+
+        assert post_id > 0
