f5cd903d01c8b8b76f43d7fe6e70b1ddb502ca40
ChaJunWoo <wnsdnxla123@gmail.com>
2025-12-22 20:35:30 +0900

    test: db_manager 글 변경 테스트 추가

--- test/db_test.py
+++ test/db_test.py
@@ -11,9 +11,9 @@ class TestDBManager:
         db.close()
 
     def test_create_post(self, db):
-        title = "test_title"
-        author = "test_author"
-        content = "this is a test"
+        title = "test"
+        author = "author"
+        content = "test content"
 
         post_id = db.create_post(title, content, author)
 
@@ -48,3 +48,20 @@ class TestDBManager:
         assert len(posts) == 4
         assert posts[0].title == "test1"
         assert posts[3].title == "test4"
+
+    def test_update_post(self, db):
+        title = "test"
+        author = "author"
+        content = "test content"
+
+        post_id = db.create_post(title, content, author)
+
+        new_title = "test_new"
+        new_content = "test content_new"
+
+        db.update_post(new_title, new_content, post_id)
+        post = db.get_post(post_id)
+
+        assert post is not None
+        assert post.title == "test_new"
+        assert post.content == "test content_new"
